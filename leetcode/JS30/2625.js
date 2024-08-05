3dca6d528304fc0fae5addffd1a8f587fb48eb73
benhuynh003 <155077782+benhuynh003@users.noreply.github.com>
2024-08-04 23:28:23 -0700

    2625.js

---
+++ leetcode/JS30/2625.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+
+/**
+ * Plan:
+ * 1.
+ * 2.
+ * 3.
+ * 4.
+ * 5.
+ */
+
+/**
+ * Pattern and concepts:
+ * 1.
+ * 2.
+ * 3.
+ * 4.
+ * 5.
+ */
+
+// var flat = function (arr, n) {};
